8c8d09de9885cfe4f3f104c29be0ad6c017ef44e
deepesh-192 <104340220+deepesh-192@users.noreply.github.com>
2022-06-03 09:27:15 +0530

    abc

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,21 @@
 const express = require('express');
-
+const underscore=require('underscore')
+// const lodash=require('lodash')
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
+    
+
+router.get('/hello', function (req, res) {
+    var y=["january","february","march", "april","may","june","july","august","september","october","november","december"]
+    var x =underscore.chunk(underscore.shuffle(y),3);
+    console.log(x)
+    res.send('my first ever api')
+    
 });
+
+
+
+
 
 module.exports = router;
 // adding this comment for no reason

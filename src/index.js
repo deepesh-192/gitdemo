b28caecaf1b0d4b915d8a899b2d19ff2042b7328
deepesh-192 <104340220+deepesh-192@users.noreply.github.com>
2022-06-16 08:37:56 +0530

    auth3

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://dkumardb:abngf_1996@cluster0.g7ksvc2.mongodb.net/auth3?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,25 @@
-const authenticate = function(req, req, next) {
-    //check the token in request header
-    //validate this token
+// const authenticate = function(req, req, next) {
+//     //check the token in request header
+//     //validate this token
+// let token=req.header["x-auth-token"]
+// if(!token){res.send({status:false,msg:"token must be present"})}
+// else{
+//     next()
+// }
+    
+// }
 
-    next()
-}
 
-
-const authorise = function(req, res, next) {
-    // comapre the logged in user's id and the id in request
-    next()
-}
+// const authorise = function(req, res, next) {
+//     // comapre the logged in user's id and the id in request
+//     let token=req.header["x-auth-token"];
+//     let decumentToken=jwt.verify(token,"functionup-radon")
+//     let modify=req.param.userid;
+//     let loginUser=decodedToken.userid
+//     if(modify !=loginUser){
+//         return res.send({status:false,msg:"modified usre must be logger"})
+//     }
+//     next()
+// }
+// module.exports.authenticate=authenticate
+// module.exports.authorise=authorise

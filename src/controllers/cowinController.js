0fd8b5a8339f8a729f8da2c9529186029b6c3c00
deepesh-192 <104340220+deepesh-192@users.noreply.github.com>
2022-06-19 15:35:04 +0530

    tree assignment

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -1,4 +1,5 @@
-let axios = require("axios")
+let axios = require("axios");
+
 
 
 let getStates = async function (req, res) {
@@ -38,6 +39,87 @@ let getDistricts = async function (req, res) {
     }
 }
 
+
+let getCovidCenters = async function (req, res) {
+    try {
+        let district_id = req.query.district_id
+        let date = req.query.date
+        console.log(`query params are:${district_id},${date}`)
+        var options = {
+            method: "get",
+            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${district_id}&date=${date}`
+        }
+        let result = await axios(options)
+        console.log(result.data)
+        res.status(200).send({ msg: result.data })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+
+let weather = async function (req, res) {
+    try {
+        let cities = ["Bengaluru", "Mumbai", "Delhi", "Kolkata", "Chennai", "London", "Moscow"]
+        let cityObjArray = [ ]
+
+        // let id = req.query.q
+        //let app=req.query.appid 
+
+        // console.log(`query params are: ${id},${app}`)
+        for (i=0; i<cities.length; i++) {
+            let obj = { city: cities[i] }
+            let resp = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${cities[i]}&appid=be789124d6fdc0299be6bcaccec19512`)
+            console.log(resp.data.main.temp)
+            obj.temp = resp.data.main.temp
+            cityObjArray.push(obj)
+        }
+        let sorted = cityObjArray.sort(function (a, b) { return a.temp - b.temp })
+        //var options = {
+        //  method: "get",
+        //url: `http://api.openweathermap.org/data/2.5/weather?q=${id}&appid=${app}`
+        console.log(sorted)
+        //let result = await axios(options)
+        // console.log(result.data)
+        res.status(200).send({status:true, data: sorted })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+
+let mame = async function (req, res) {
+    try {
+
+        // let id = req.query.template_id
+        // let text=req.query.text0
+        // let text1=req.query.text1
+
+        //console.log(`query params are: ${id},${text},${text1}`)
+        let options = {
+            method: "post",
+            url: `https://api.imgflip.com/caption_image?template_id=181913649&text0=function&text1=yes&username=chewie12345&password=meme@123`
+
+        }
+        let result = await axios(options)
+        // console.log(result.data)
+        res.send({ data: result.data })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+
+
+
+
+
 let getByPin = async function (req, res) {
     try {
         let pin = req.query.pincode
@@ -57,10 +139,12 @@ let getByPin = async function (req, res) {
     }
 }
 
+
+
 let getOtp = async function (req, res) {
     try {
         let blahhh = req.body
-        
+
         console.log(`body is : ${blahhh} `)
         var options = {
             method: "post",
@@ -83,3 +167,6 @@ module.exports.getStates = getStates
 module.exports.getDistricts = getDistricts
 module.exports.getByPin = getByPin
 module.exports.getOtp = getOtp
+module.exports.getCovidCenters = getCovidCenters
+module.exports.weather = weather
+module.exports.mame = mame
